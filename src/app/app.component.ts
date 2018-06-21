67c5ce742524c0e93783a9a4104f19806f6cea27
Poul H. Hansen <poul.h.hansen@gmail.com>
2018-06-21 21:11:54 +0200

    chore(): Add content for prev step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "frontenders";
+  title: string = "frontenders";
+  isHappy: boolean = true;
+  numberOne: number = 45;
+  numberTwo: number = 1;
 }
