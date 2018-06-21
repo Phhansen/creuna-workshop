a1aa0a9b62602b810d6ca21107a59d21ffcf5816
Poul H. Hansen <poul.h.hansen@gmail.com>
2018-06-21 21:42:06 +0200

    chore(): update prev step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,23 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title: string = "frontenders";
+  title: string = "Hello, frontenders";
   isHappy: boolean = true;
   numberOne: number = 45;
   numberTwo: number = 1;
+
+  logo: string = "assets/logo.svg";
+  name: string = "Poul";
+
+  handleBlur(event: any) {
+    this.name = event.target.value;
+  }
+
+  handleInput(event: any) {
+      this.name = event.target.value;
+  }
+
+  handleClick() {
+      this.name = "Holger";
+  }
 }
