b1986ae0ab15874e8e086466bdb1a4ecf9a5fcd2
Poul H. Hansen <poul.h.hansen@gmail.com>
2018-06-21 22:43:50 +0200

    chore(): update branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,34 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name: string = "Poul";
+  name: string = "";
+
+  players = [
+    {
+        id: 1,
+        name: "Stephen",
+        matchesPlayed: 134,
+        pro: true
+    },
+    {
+        id: 2,
+        name: "Roger",
+        matchesPlayed: 23,
+        pro: false
+    },
+    {
+        id: 3,
+        name: "Anthony",
+        matchesPlayed: 159,
+        pro: true
+    },
+    {
+        id: 4,
+        name: "Mark",
+        matchesPlayed: 12,
+        pro: false
+    },
+  ];
 
   handleChange(value: string) {
     this.name = value;
