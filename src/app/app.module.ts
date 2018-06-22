9caddce9a2aad11239cec3ade92ad8da270044d3
Poul H. Hansen <poul.h.hansen@gmail.com>
2018-06-22 09:48:03 +0200

    chore(): add in-memory-service
    + Added the in-memory-web-api service for later use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+// Mocking http calls
+import { InMemPlayerService } from "./in-memory-db-service.service";
 
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
@@ -10,7 +15,9 @@ import { MyComponentComponent } from './my-component/my-component.component';
     MyComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemPlayerService)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/player.model.ts
@@ -0,0 +1,6 @@
+export interface Player {
+    id: number;
+    name: string;
+    matchesPlayed: number;
+    pro: boolean;
+}
